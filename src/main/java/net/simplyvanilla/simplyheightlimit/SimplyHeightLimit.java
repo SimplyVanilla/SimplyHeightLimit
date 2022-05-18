0da19d09bd4b4969d0c16c34ef13f553ac5ef453
CrazyGamesMC <johndee3a@gmail.com>
2022-05-18 14:58:44 +0200

    Added a configuration file

--- src/main/java/net/simplyvanilla/simplyheightlimit/SimplyHeightLimit.java
+++ src/main/java/net/simplyvanilla/simplyheightlimit/SimplyHeightLimit.java
@@ -1,15 +1,24 @@
 package net.simplyvanilla.simplyheightlimit;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
 
 public final class SimplyHeightLimit extends JavaPlugin {
 
     private HeightLimiter limiter;
+    private FileConfiguration config;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        initConfig();
         initLimiter();
 
     }
@@ -20,7 +29,40 @@ public final class SimplyHeightLimit extends JavaPlugin {
     }
 
     private void initLimiter() {
-        this.limiter = new HeightLimiter(256, 60, 0.5, this);
+
+        int maxHeight = config.getInt("limit");
+        double damage = config.getDouble("damage");
+        double intervalSeconds = config.getDouble("interval");
+
+        int intervalTicks = (int) (20.0 * intervalSeconds); //Converting seconds to ticks
+
+        this.limiter = new HeightLimiter(maxHeight, intervalTicks, damage, this);
         this.limiter.startAsyncChecking();
+    }
+
+    private void initConfig() {
+        try {
+            this.config = loadConfig();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Bukkit.getLogger().log(Level.SEVERE, "Failed to load config.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
+    private FileConfiguration loadConfig() throws IOException {
+        File dataFolder = getDataFolder();
+
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+
+        File configFile = new File(dataFolder, "config.yml");
+
+        if (!configFile.exists()) {
+            Files.copy(getClassLoader().getResourceAsStream("config.yml"), configFile.toPath());
+        }
+
+        return YamlConfiguration.loadConfiguration(configFile);
     }
 }
