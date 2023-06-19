bebb81a2cafe525a42262e367e51d8ac073ad163
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-19 09:27:07 +0200

    refactor: use minimessages instead of chatcolors
    - I also reformated the code with google java guidelines
    - I replaced the copy config.yml code with bukkit's saveResource method

--- src/main/java/net/simplyvanilla/simplyheightlimit/HeightLimiter.java
+++ src/main/java/net/simplyvanilla/simplyheightlimit/HeightLimiter.java
@@ -1,58 +1,58 @@
 package net.simplyvanilla.simplyheightlimit;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HeightLimiter {
 
-    private final int maxHeight;
-    private final JavaPlugin plugin;
-    private final int period;
-    private final double damage;
+  private final int maxHeight;
+  private final JavaPlugin plugin;
+  private final int period;
+  private final double damage;
 
-    public HeightLimiter(int maxHeight, int period, double damage, JavaPlugin plugin) {
-        this.maxHeight = maxHeight;
-        this.plugin = plugin;
-        this.period = period;
-        this.damage = damage;
+  public HeightLimiter(int maxHeight, int period, double damage, JavaPlugin plugin) {
+    this.maxHeight = maxHeight;
+    this.plugin = plugin;
+    this.period = period;
+    this.damage = damage;
+  }
+
+  public void checkHeightOnAll() {
+    var players = Bukkit.getOnlinePlayers();
+
+    for (Player p : players) {
+      checkAndAdjustPlayer(p);
     }
+  }
 
-    public void checkHeightOnAll() {
-        var players = Bukkit.getOnlinePlayers();
+  private void checkAndAdjustPlayer(Player p) {
+    var loc = p.getLocation();
+    int height = loc.getBlockY();
 
-        for (Player p : players) {
-            checkAndAdjustPlayer(p);
-        }
-    }
+    if (height > maxHeight) {
+      var newLoc = new Location(loc.getWorld(), loc.getBlockX(), maxHeight, loc.getBlockZ());
+      newLoc.setYaw(loc.getYaw());
+      newLoc.setPitch(loc.getPitch());
 
-    private void checkAndAdjustPlayer(Player p) {
-        var loc = p.getLocation();
-        int height = loc.getBlockY();
-
-        if (height > maxHeight) {
-            var newLoc = new Location( loc.getWorld(), loc.getBlockX(), maxHeight, loc.getBlockZ());
-            newLoc.setYaw(loc.getYaw());
-            newLoc.setPitch(loc.getPitch());
-
-            //Going back to sync to access bukkit APIs
-            Bukkit.getScheduler().runTask(this.plugin, () -> {
-                p.sendMessage(ChatColor.RED + "You are too high!");
+      // Going back to sync to access bukkit APIs
+      Bukkit.getScheduler()
+          .runTask(
+              this.plugin,
+              () -> {
+                p.sendMessage(MiniMessage.miniMessage().deserialize("<red>You are too high!"));
                 p.teleport(newLoc);
                 p.setFlying(false);
                 p.damage(this.damage);
                 p.setGliding(false);
-            });
-        }
+              });
     }
+  }
 
-
-    public void startAsyncChecking() {
-        Bukkit.getScheduler().runTaskTimerAsynchronously(this.plugin, () -> {
-            checkHeightOnAll();
-        }, 0, this.period);
-    }
-
+  public void startAsyncChecking() {
+    Bukkit.getScheduler()
+        .runTaskTimerAsynchronously(this.plugin, this::checkHeightOnAll, 0, this.period);
+  }
 }
