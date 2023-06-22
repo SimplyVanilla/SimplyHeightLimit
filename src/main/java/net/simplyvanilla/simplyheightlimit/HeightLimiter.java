7cdccc2ac4e2f1ae8512b1ea8c4fc74526458bf1
Netherwhal <mail@netherwhal.com>
2023-06-22 14:50:27 +0200

    fix indent

--- src/main/java/net/simplyvanilla/simplyheightlimit/HeightLimiter.java
+++ src/main/java/net/simplyvanilla/simplyheightlimit/HeightLimiter.java
@@ -8,51 +8,51 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HeightLimiter {
 
-  private final int maxHeight;
-  private final JavaPlugin plugin;
-  private final int period;
-  private final double damage;
+    private final int maxHeight;
+    private final JavaPlugin plugin;
+    private final int period;
+    private final double damage;
 
-  public HeightLimiter(int maxHeight, int period, double damage, JavaPlugin plugin) {
-    this.maxHeight = maxHeight;
-    this.plugin = plugin;
-    this.period = period;
-    this.damage = damage;
-  }
-
-  public void checkHeightOnAll() {
-    var players = Bukkit.getOnlinePlayers();
-
-    for (Player p : players) {
-      checkAndAdjustPlayer(p);
+    public HeightLimiter(int maxHeight, int period, double damage, JavaPlugin plugin) {
+        this.maxHeight = maxHeight;
+        this.plugin = plugin;
+        this.period = period;
+        this.damage = damage;
     }
-  }
 
-  private void checkAndAdjustPlayer(Player p) {
-    var loc = p.getLocation();
-    int height = loc.getBlockY();
+    public void checkHeightOnAll() {
+        var players = Bukkit.getOnlinePlayers();
 
-    if (height > maxHeight) {
-      var newLoc = new Location(loc.getWorld(), loc.getBlockX(), maxHeight, loc.getBlockZ());
-      newLoc.setYaw(loc.getYaw());
-      newLoc.setPitch(loc.getPitch());
-
-      // Going back to sync to access bukkit APIs
-      Bukkit.getScheduler()
-          .runTask(
-              this.plugin,
-              () -> {
-                p.sendMessage(MiniMessage.miniMessage().deserialize("<red>You are too high!"));
-                p.teleport(newLoc);
-                p.setFlying(false);
-                p.damage(this.damage);
-                p.setGliding(false);
-              });
+        for (Player p : players) {
+            checkAndAdjustPlayer(p);
+        }
     }
-  }
 
-  public void startAsyncChecking() {
-    Bukkit.getScheduler()
-        .runTaskTimerAsynchronously(this.plugin, this::checkHeightOnAll, 0, this.period);
-  }
+    private void checkAndAdjustPlayer(Player p) {
+        var loc = p.getLocation();
+        int height = loc.getBlockY();
+
+        if (height > maxHeight) {
+            var newLoc = new Location(loc.getWorld(), loc.getBlockX(), maxHeight, loc.getBlockZ());
+            newLoc.setYaw(loc.getYaw());
+            newLoc.setPitch(loc.getPitch());
+
+            // Going back to sync to access bukkit APIs
+            Bukkit.getScheduler()
+                .runTask(
+                    this.plugin,
+                    () -> {
+                        p.sendMessage(MiniMessage.miniMessage().deserialize("<red>You are too high!"));
+                        p.teleport(newLoc);
+                        p.setFlying(false);
+                        p.damage(this.damage);
+                        p.setGliding(false);
+                    });
+        }
+    }
+
+    public void startAsyncChecking() {
+        Bukkit.getScheduler()
+            .runTaskTimerAsynchronously(this.plugin, this::checkHeightOnAll, 0, this.period);
+    }
 }
