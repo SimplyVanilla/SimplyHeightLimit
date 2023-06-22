acedc8c902cbf7f47722978886e0792d071a42bf
Emmanuel Lampe <rexlmanude@gmail.com>
2023-06-22 14:41:01 +0200

    refactor: use minimessages instead of chatcolors (#13)
    - I also reformated the code with google java guidelines
    - I replaced the copy config.yml code with bukkit's saveResource method

--- src/main/java/net/simplyvanilla/simplyheightlimit/SimplyHeightLimit.java
+++ src/main/java/net/simplyvanilla/simplyheightlimit/SimplyHeightLimit.java
@@ -12,57 +12,50 @@ import java.util.logging.Level;
 
 public final class SimplyHeightLimit extends JavaPlugin {
 
-    private HeightLimiter limiter;
-    private FileConfiguration config;
+  private FileConfiguration config;
 
-    @Override
-    public void onEnable() {
+  @Override
+  public void onEnable() {
 
-        initConfig();
-        initLimiter();
+    initConfig();
+    initLimiter();
+  }
 
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+
+  private void initLimiter() {
+    HeightLimiter limiter;
+
+    int maxHeight = config.getInt("limit");
+    double damage = config.getDouble("damage");
+    double intervalSeconds = config.getDouble("interval");
+
+    int intervalTicks = (int) (20.0 * intervalSeconds); // Converting seconds to ticks
+
+    limiter = new HeightLimiter(maxHeight, intervalTicks, damage, this);
+    limiter.startAsyncChecking();
+  }
+
+  private void initConfig() {
+    this.config = loadConfig();
+  }
+
+  private FileConfiguration loadConfig() {
+    File dataFolder = getDataFolder();
+
+    if (!dataFolder.exists()) {
+      dataFolder.mkdirs();
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    File configFile = new File(dataFolder, "config.yml");
+
+    if (!configFile.exists()) {
+      this.saveResource("config.yml", false);
     }
 
-    private void initLimiter() {
-
-        int maxHeight = config.getInt("limit");
-        double damage = config.getDouble("damage");
-        double intervalSeconds = config.getDouble("interval");
-
-        int intervalTicks = (int) (20.0 * intervalSeconds); //Converting seconds to ticks
-
-        this.limiter = new HeightLimiter(maxHeight, intervalTicks, damage, this);
-        this.limiter.startAsyncChecking();
-    }
-
-    private void initConfig() {
-        try {
-            this.config = loadConfig();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Bukkit.getLogger().log(Level.SEVERE, "Failed to load config.");
-            getServer().getPluginManager().disablePlugin(this);
-        }
-    }
-
-    private FileConfiguration loadConfig() throws IOException {
-        File dataFolder = getDataFolder();
-
-        if (!dataFolder.exists()) {
-            dataFolder.mkdirs();
-        }
-
-        File configFile = new File(dataFolder, "config.yml");
-
-        if (!configFile.exists()) {
-            Files.copy(getClassLoader().getResourceAsStream("config.yml"), configFile.toPath());
-        }
-
-        return YamlConfiguration.loadConfiguration(configFile);
-    }
+    return YamlConfiguration.loadConfiguration(configFile);
+  }
 }
